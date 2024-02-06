7903379bfbdf127fc6774d483d6fb6aa9b7032a1
MatteoOliva <oliva.matteo98@outlook.com>
2024-02-06 14:18:50 +0100

    feat: generato le emails

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 const { createApp } = Vue;
 
 createApp({
-    
+
     data() {
+        return {
+            emails: [],
+        }
 
     },
+
+    mounted() {
+
+        for (let i = 0; i < 10; i++){
+            axios.
+            get("https://flynn.boolean.careers/exercises/api/random/mail").
+            then((response) => {
+                this.emails.push(response.data.response)
+                
+            })
+            
+        }
+        console.log(this.emails)
+    },
+
+       
+         
+
+    
    
 
 }).mount('#app')
